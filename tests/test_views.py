a1039c2e38243b64d2027621aa87ee020636f23b
Jonathan Chu <jonathan.chu@me.com>
2012-06-20 03:23:13 -0400

    Add initial test for routes.

---
+++ tests/test_views.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import os
+import sys
+sys.path.insert(0, os.path.abspath('..'))
+
+import website
+import unittest
+import tempfile
+
+class FPOTestCase(unittest.TestCase):
+
+    def test_homepage(self):
+        self.app = website.app.test_client()
+        resp = self.app.get('/')
+        self.assertEqual(resp.status_code, 200)
+
+    def test_admin(self):
+        self.app = website.app.test_client()
+        resp = self.app.get('/admin/')
+        self.assertEqual(resp.status_code, 200)
+
+if __name__ == '__main__':
+    unittest.main()
